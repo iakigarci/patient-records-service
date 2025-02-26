82da530369d050d827e7be36d48399b9afc98cd2
iakigarci <garcianoya@hotmail.es>
2025-02-26 11:54:09 +0100

    change user entity with credentials and optional parameters

--- internal/domain/models/entities/user.go
+++ internal/domain/models/entities/user.go
@@ -1,18 +1,25 @@
 package entities
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 )
 
 type User struct {
 	ID        uuid.UUID `db:"id" json:"id"`
 	Name      string    `db:"name" json:"name"`
-	CreatedAt string    `db:"created_at" json:"created_at"`
-	UpdatedAt string    `db:"updated_at" json:"updated_at"`
-	Credentials
+	Email     string    `db:"email" json:"email"`
+	Password  string    `db:"password" json:"password"`
+	Phone     *string   `db:"phone" json:"phone"`
+	Address   *Address  `db:"address" json:"address"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
 }
 
-type Credentials struct {
-	Email    string `db:"email" json:"email"`
-	Password string `db:"password" json:"password"`
+type Address struct {
+	Street string `db:"street" json:"street"`
+	City   string `db:"city" json:"city"`
+	State  string `db:"state" json:"state"`
+	Zip    string `db:"zip" json:"zip"`
 }
